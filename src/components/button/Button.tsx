e4f2b8153251472e41fd302cf46bae342a791cbe
Вечканов Алексей Сергеевич <avechkanov@alfabank.ru>
2021-12-05 12:53:43 +0300

    remove an image

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import mainImage from './assets/image.png';
-
 import styles from './styles.module.css';
 
 type Props = {
@@ -11,11 +9,9 @@ type Props = {
 
 export function Button(props: Props): JSX.Element {
   const { children, ...rest } = props;
-  console.log('mainImage', mainImage);
   return (
     <button className={ styles.button } { ...rest }>
       { children }
-      <img src={ mainImage } alt="image" />
     </button>
   )
 }

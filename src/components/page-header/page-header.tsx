a26c12c6cb85d85694caa8cf880a73ed5104d88a
Вечканов Алексей Сергеевич <avechkanov@alfabank.ru>
2021-12-10 05:17:17 +0300

    header component changes

--- src/components/page-header/page-header.tsx
+++ src/components/page-header/page-header.tsx
@@ -1,28 +1,27 @@
-import React, { PureComponent, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
+import { ContentContainer } from '../content-container';
 import { Logo } from '../../components/logo/logo';
 import { HeaderNavigation } from '../../components/header-navigation';
-// import { HeaderUser } from '../../src/components/header-user';
 
 import styles from './styles.module.scss';
 
-interface Props {
+type Props = {
   authContent?: any;
 }
-interface State { }
 
-export class PageHeader extends PureComponent<Props, State> {
-    render(): ReactNode {
-        const { authContent } = this.props
-        return (
-            <header className={styles.header}>
-                <Logo />
-                <HeaderNavigation />
-                  {/* <HeaderUser /> */}
-                <div className={styles.marginLeftAuto}>
-                  { authContent }
-                </div>
-            </header>
-        );
-    }
+export function PageHeader(props: Props): ReactNode {
+    const { authContent } = props;
+
+    return (
+      <ContentContainer>
+        <header className={styles.header}>
+            <Logo />
+            <HeaderNavigation />
+            <div className={styles.marginLeftAuto}>
+              { authContent }
+            </div>
+        </header>
+      </ContentContainer>
+  );
 }

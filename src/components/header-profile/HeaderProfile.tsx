fd80ac3b930fe342b9fcc8fa292ee3dbd658c331
Вечканов Алексей Сергеевич <avechkanov@alfabank.ru>
2021-12-10 04:47:15 +0300

    small changes

--- src/components/header-profile/HeaderProfile.tsx
+++ src/components/header-profile/HeaderProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './styles.module.scss';
 
@@ -13,8 +13,37 @@ export function HeaderProfile(props: Props): JSX.Element {
 
   const [isMenuOpen, setMenuState] = useState(false);
 
+  const onBodyClick = (event: any) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const isSelfClicked = !!event.target.closest("#HeaderProfile");
+
+    if (isMenuOpen && !isSelfClicked) {
+      setMenuState(false);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const body = window.document.querySelector('body');
+
+      body?.addEventListener('click', onBodyClick);
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        const body = window.document.querySelector('body');
+        body?.removeEventListener('click', onBodyClick);
+      }
+    }
+  });
+
   return (
-    <div className={ `${styles.headerProfile} ${isMenuOpen ? styles.menuActive : ''}` }>
+    <div
+      id="HeaderProfile"
+      className={ `${styles.headerProfile} ${isMenuOpen ? styles.menuActive : ''}` }
+    >
       <div className={ styles.headerProfileInner }>
         <button onClick={ () => { setMenuState(!isMenuOpen) } }>
           <div className={ styles.avatar }>

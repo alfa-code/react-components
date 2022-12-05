751e7dd8b92311414e4b479af0e786f855c2750d
Вечканов Алексей <avechkanov@alfabank.ru>
2022-12-05 22:42:46 +0300

    change RU to COM

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,5 +4,5 @@ export default create({
   base: 'light',
   brandTitle: 'Alfa-Code React Components',
   brandUrl: 'https://github.com/alfa-code/react-components',
-  brandImage: 'https://alfa-code.ru/assets/svg/logo.svg',
+  brandImage: 'https://alfa-code.com/assets/svg/logo.svg',
 });
